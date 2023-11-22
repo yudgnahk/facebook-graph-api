9b1b82795827295fc27c4be9aa67d6c045ae8fb7
Kelvin Ha <haleduykhang@gmail.com>
2023-11-22 12:43:55 +0700

    facebook batch API

---
+++ v17/common/batch.go
@@ -0,0 +1,39 @@
+package common
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/yudgnahk/facebook-graph-api/v17/models"
+	httputils "github.com/yudgnahk/go-common-utils/http"
+	"net/http"
+)
+
+func (c *Client) BatchRequest(requests []models.BatchRequest) ([]models.BatchResponse, error) {
+	url := c.PrepareUrl("", http.MethodPost)
+
+	body := map[string]interface{}{
+		"batch": requests,
+	}
+
+	batchRequestBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := httputils.NewRequest(http.MethodPost, url, bytes.NewBuffer(batchRequestBody))
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Authorization", "Bearer "+c.AccessToken)
+
+	var response []models.BatchResponse
+	err = httputils.Execute(request, &response)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
